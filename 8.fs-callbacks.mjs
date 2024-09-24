faa7ce3f8d05a9f88d9c81b7a3c10410255e2833
manuelguirado <85970925+manuelguirado@users.noreply.github.com>
2024-09-24 12:19:15 +0200

    read files with callbacks

---
+++ 8.fs-callbacks.mjs
@@ -0,0 +1,10 @@
+import {readFile} from 'node:fs';
+
+readFile('archivo.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+})
+//with callbacks
